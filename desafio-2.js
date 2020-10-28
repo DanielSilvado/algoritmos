133c632c9421f8ccb72cbf32fd79faf4b1f3d0cf
Daniel <danishotsilvado@gmail.com>
2020-10-27 21:10:21 -0300

    resolve challenge 2

---
+++ desafio-2.js
@@ -0,0 +1,11 @@
+const targetSearch = (arr, target) => {
+  const mapValues = {};
+  for (let i = 0; i < arr.length; i++) {
+    let value = arr[i];
+    let dual = target - value;
+    if (mapValues.propertyIsEnumerable(dual))
+      return console.log(`Índices: ${[mapValues[dual], i]}`);
+    mapValues[value] = i;
+  }
+  return console.log("Valor não encontrado");
+}
